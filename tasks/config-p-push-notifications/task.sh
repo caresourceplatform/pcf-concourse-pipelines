706c9eff474f0660bbcab2c06e8dd0d766c75eca
caresourceplatform <chad@4004183L.caresource.corp>
2018-06-20 10:34:26 -0400

    changed redis service plan

--- tasks/config-p-push-notifications/task.sh
+++ tasks/config-p-push-notifications/task.sh
@@ -46,7 +46,7 @@ properties_config=$($JQ_CMD -n \
   --arg redis_analytics_external_host "${REDIS_ANALYTICS_EXTERNAL_HOST}" \
   --arg redis_analytics_external_password "${REDIS_ANALYTICS_EXTERNAL_PASSWORD}" \
   --arg redis_analytics_external_port "${REDIS_ANALYTICS_EXTERNAL_PORT}" \
-  --arg redis_analytics_internal_service_plan "${REDIS_ANALYTICS_INTERNAL_SERVICE_PLAN:-"dedicated-vm"}" \
+  --arg redis_analytics_internal_service_plan "${REDIS_ANALYTICS_INTERNAL_SERVICE_PLAN:-"shared-vm"}" \
   --arg push_push_notifications_api_db_encryption_key "${PUSH_PUSH_NOTIFICATIONS_API_DB_ENCRYPTION_KEY}" \
   --arg push_push_notifications_default_system_tenant_name "${PUSH_PUSH_NOTIFICATIONS_DEFAULT_SYSTEM_TENANT_NAME}" \
 '{
